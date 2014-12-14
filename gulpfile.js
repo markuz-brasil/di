23679f259b1a22b49a3e3e801d14e108e6951f8a
Markuz GJ <markuz.br@gmail.com>
2014-12-14 17:14:05 -0200

    removed dep on traceur/requirejs and AtScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,71 @@
+'use strict'
+
 var gulp = require('gulp');
-var pipe = require('pipe/gulp');
-var traceur = require('gulp-traceur');
-var connect = require('gulp-connect');
+var $ = require('gulp-load-plugins')();
+var del = require('del');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
+var watch = require('./etc/watchers');
+var CFG = require('./etc/config');
 
-var path = {
-  src: './src/**/*.js',
-  // we have to skip example/node (because of the cyclic symlink)
-  examples: './example/!(node)/**/*.js',
-  pkg: './package.json'
-};
+var log = $.util.log
+var red = $.util.colors.red
+var cyan = $.util.colors.cyan
+var mag = $.util.colors.magenta
 
+// Load custom tasks from the `tasks` directory
+try { require('require-dir')('etc'); } catch (err) { console.error(err) }
 
-// TRANSPILE ES6
-gulp.task('build_source_amd', function() {
-  gulp.src(path.src)
-      .pipe(traceur(pipe.traceur()))
-      .pipe(gulp.dest('dist/amd'));
-});
+// Clean Output Directory
+// gulp.task('clean', del.bind(null, [PUB], {force: true}));
+// gulp.task('clean:all', del.bind(null, [TMP], {force: true}));
 
-gulp.task('build_source_cjs', function() {
-  gulp.src(path.src)
-      .pipe(traceur(pipe.traceur({modules: 'commonjs'})))
-      .pipe(gulp.dest('dist/cjs'));
-});
+// TODO: add comments
+gulp.task('default', ['build'])
 
-gulp.task('build_examples', function() {
-  gulp.src(path.examples)
-      .pipe(traceur(pipe.traceur()))
-      .pipe(gulp.dest('compiled/example'));
-});
+// TODO: add comments
+gulp.task('build', ['clean'], function(next){
+  runSequence('assets', function(){
+    if (browserSync.active) { gulp.start('reload') }
+    next()
+  })
+})
 
-gulp.task('build_dist', ['build_source_cjs', 'build_source_amd']);
-gulp.task('build', ['build_dist', 'build_examples']);
+var BS
+// TODO: add comments
+gulp.task('serve', function (next) {
+  CFG.browserSync.browser = CFG.browserSync.browser || 'skip'
 
-
-// WATCH FILES FOR CHANGES
-gulp.task('watch', function() {
-  gulp.watch(path.src, ['build']);
-});
-
-
-// WEB SERVER
-gulp.task('serve', connect.server({
-  root: [__dirname],
-  port: 8000,
-  open: {
-    browser: 'Google Chrome'
-  }
-}));
-
-
-
-
-// This is a super nasty, hacked release task.
-// TODO(vojta): fix gulp-git and clean this up
-var git = require('gulp-git');
-var through = require('through2');
-var exec = require('child_process').exec;
-
-var VERSION_REGEXP = /([\'|\"]?version[\'|\"]?[ ]*:[ ]*[\'|\"]?)([\d||A-a|.|-]*)([\'|\"]?)/i;
-gulp.task('release', ['build_dist'], function() {
-  var incrementedVersion;
-
-  // increment version
-  gulp.src(path.pkg)
-    .pipe(through.obj(function(file, _, done) {
-      var incrementedVersion;
-      var content = file.contents.toString().replace(VERSION_REGEXP, function(_, prefix, parsedVersion, suffix) {
-        incrementedVersion = parsedVersion.replace(/pre-(\d+)/, function(_, number) {
-          return 'pre-' + (parseInt(number, 10) + 1);
-        });
-
-        return prefix + incrementedVersion + suffix;
-      });
-
-    // TODO(vojta): we should rather create a new file object
-    file.contents = new Buffer(content);
-    file.meta = {
-      incrementedVersion: incrementedVersion
-    };
-
-    this.push(file);
-    done();
-  }))
-  .pipe(gulp.dest('.'))
-  .pipe(git.commit('chore(release): <%= incrementedVersion %>'))
-  .on('commit_done', function() {
-    git.push('upstream', 'master');
-    exec('npm publish --tag v2', function() {
-      exec('npm tag di@0.0.1 latest');
-    });
+  browserSync(CFG.browserSync, function(err, bs){
+    if (err) {throw err}
+    BS = bs
+    log("Loaded '"+ cyan('browserSync') +"'...")
+    next()
   });
 });
+
+// TODO: add comments
+gulp.task('reload', function(next){
+
+  if (!CFG.tasks.serve) return next()
+
+  browserSync.reload()
+  BS.logger.info('Local URL: '+ mag(BS.options.urls.local))
+  BS.logger.info('External URL: '+ mag(BS.options.urls.external))
+  next()
+});
+
+// TODO: add comments
+gulp.task('restart', function(){
+  log(red(':: restarting ::'))
+  process.exit(0)
+})
+
+// // TODO: add comments
+gulp.task('watch', function(next){
+  watch.gulpfile()
+  watch.assets()
+  next()
+})
